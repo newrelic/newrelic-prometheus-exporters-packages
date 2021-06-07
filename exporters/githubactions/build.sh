8b898198e852f1133926f34313c1a79608921091
ivancorrales <ivan.corrales.solera@gmail.com>
2021-06-07 15:59:15 +0200

    doc: update LICENSE

--- exporters/githubactions/build.sh
+++ exporters/githubactions/build.sh
@@ -2,12 +2,15 @@
 root_dir=$1
 integration_dir="${root_dir}/exporters/githubactions"
 
-repository_path="https://github.com/Spendesk/github-actions-exporter"
-exporter_version="1.2.6"
+source ${root_dir}/scripts/common_functions.sh
+EXPORTER_PATH="${integration_dir}/exporter.yml"
+loadVariables
 
 tmp_dir=$(mktemp -d)
-git clone ${repository_path} "${tmp_dir}"
+git clone ${EXPORTER_REPO_URL} "${tmp_dir}"
 cd "${tmp_dir}"
-git checkout ${exporter_version}
+git checkout ${VERSION}
+
+
 GOOS=linux GOARCH=amd64 go build -v -o "${integration_dir}/target/bin/githubactions-exporter"
 rm -rf "${tmp_dir}"
--- exporters/ravendb/build.sh
+++ exporters/ravendb/build.sh
@@ -2,8 +2,10 @@
 root_dir=$1
 integration_dir="${root_dir}/exporters/ravendb"
 
-repository_path="https://github.com/marcinbudny/ravendb_exporter"
-exporter_version="0.3.0"
+source ${root_dir}/scripts/common_functions.sh
+EXPORTER_PATH="${integration_dir}/exporter.yml"
+loadVariables
+
 
 if [ ! -d "${GOPATH}" ]; then
   echo "GOPATH is empty" ;
@@ -13,11 +15,16 @@ fi
 
 tmp_dir="${GOPATH}/src/github.com/marcinbudny/ravendb_exporter"
 echo "cloning repository into ${tmp_dir}..."
-git clone ${repository_path} "${tmp_dir}"
+git clone ${EXPORTER_REPO_URL} "${tmp_dir}"
 cd "${tmp_dir}"
-git checkout ${exporter_version}
-echo "checkout version ${exporter_version}"
+git checkout ${VERSION}
+echo "checkout version ${VERSION}"
 echo "building the exporter..."
+
+
 GO111MODULE=off GOOS=linux GOARCH=amd64 go build -v -o "${integration_dir}/target/bin/ravendb-exporter"
 echo "executable file created ${integration_dir}/target/bin/ravendb-exporter"
+
+
+
 rm -rf "${tmp_dir}"
