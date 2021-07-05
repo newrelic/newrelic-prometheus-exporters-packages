0fa3fe430966563fe86e567b0ee56972fa856028
ivancorrales <ivan.corrales.solera@gmail.com>
2021-07-05 14:49:46 +0200

    fix issue when exporter needs to be built

--- exporters/powerdns/build.sh
+++ exporters/powerdns/build.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 root_dir=$1
-integration_dir="${root_dir}/exporters/githubactions"
+integration_dir="${root_dir}/exporters/powerdns"
 tmp_dir=$(mktemp -d)
 git clone ${EXPORTER_REPO_URL} "${tmp_dir}"
 cd "${tmp_dir}"
 
 if [[ -z $EXPORTER_TAG ]]
 then
-    git switch ${EXPORTER_COMMIT} -d;
+    git -c advice.detachedHead=false checkout ${EXPORTER_COMMIT}
 else
     git checkout ${EXPORTER_TAG} -d
 fi
 
-make
+make build
+
+mv "${tmp_dir}/powerdns_exporter" "${integration_dir}/target/bin/powerdns-exporter"
+
+cd ../
+
 rm -rf "${tmp_dir}"
+
