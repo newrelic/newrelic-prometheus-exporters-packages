e497ca3a879bc609bde717900a2a7aed1be5db1b
paologallinaharbur <paologallina1992@gmail.com>
2021-07-09 10:07:21 +0200

    fix(tests): fix tests after refactor

--- nri-config-generator/synthesis/synthesis_test.go
+++ nri-config-generator/synthesis/synthesis_test.go
@@ -7,19 +7,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_ProcessSynthesisDefinitionsNoSynthesisBlock(t *testing.T){
-	in,err:=ioutil.ReadFile("testdata/sample.yml")
+func Test_ProcessSynthesisDefinitionsNoSynthesisBlock(t *testing.T) {
+	in, err := ioutil.ReadFile("testdata/sample.yml")
 	assert.Nil(t, err)
-	res,err:=ProcessSynthesisDefinitions(in)
+	res, err := ProcessSynthesisDefinitions(in)
 	assert.NotNil(t, err)
-	assert.Equal(t, "missing required field 'synthesis' in definition",err.Error())
-	assert.Empty(t,res)
+	assert.Equal(t, "missing required field 'synthesis' in definition", err.Error())
+	assert.Empty(t, res)
 }
 
-func Test_ProcessSynthesisDefinitions(t *testing.T){
-	in,err:=ioutil.ReadFile("testdata/sample2.yml")
+func Test_ProcessSynthesisDefinitions(t *testing.T) {
+	in, err := ioutil.ReadFile("testdata/sample2.yml")
 	assert.Nil(t, err)
-	res,err:=ProcessSynthesisDefinitions(in)
+	res, err := ProcessSynthesisDefinitions(in)
 	assert.Nil(t, err)
-	assert.Equal(t,  "\"entity_definitions\":[{\"conditions\":[{\"attribute\":\"metricName\",\"prefix\":\"redis_\"}],\"encodeIdentifierInGUID\":true,\"identifier\":\"targetName\",\"name\":\"targetName\",\"tags\":{\"clusterName\":null,\"targetName\":null},\"type\":\"REDIS\"}]", res)
+	assert.Equal(t, "[{\"conditions\":[{\"attribute\":\"metricName\",\"prefix\":\"redis_\"}],\"encodeIdentifierInGUID\":true,\"identifier\":\"targetName\",\"name\":\"targetName\",\"tags\":{\"clusterName\":null,\"targetName\":null},\"type\":\"REDIS\"}]", res)
 }
