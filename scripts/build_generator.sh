fa1419b821f5cef938a10535e280691d00510fb0
Carlos Castro <ccastro@newrelic.com>
2022-10-12 16:14:18 +0100

    add sprig templating

--- scripts/build_generator.sh
+++ scripts/build_generator.sh
@@ -8,12 +8,22 @@ integration_dir="${root_dir}/exporters/${integration}"
 binary_dir="${root_dir}/exporters/${integration}/target/bin"
 template_name=nri-${integration}.json.tmpl
 template_path="${integration_dir}/${integration}.json.tmpl"
+config_name=nri-${integration}.prometheus.json.tmpl
+config_path="${integration_dir}/${integration}.prometheus.json.tmpl"
 
 cp "${template_path}" "${root_dir}/nri-config-generator/templates/${template_name}"
 
+if [ -f "$config_path" ]; then
+  cp "${config_path}" "${root_dir}/nri-config-generator/templates/${config_name}"
+  echo "Using prometheus template $config_path"
+else
+  cp "${root_dir}/nri-config-generator/templates/default/config.json.tmpl" "${root_dir}/nri-config-generator/templates/${config_name}"
+  echo "Using Default prometheus template"
+fi
+
 cd nri-config-generator && \
-  GOOS=linux \
-  GOARCH=amd64 \
+  GOOS=darwin \
+  GOARCH=arm64 \
   BIN_PATH=${binary_dir}/nri-${integration} \
   make compile 
 
