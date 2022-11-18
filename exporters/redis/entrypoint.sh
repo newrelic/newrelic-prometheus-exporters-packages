a81a2c65b3f358913ed73e153b5c0f4bb4f66263
alvarocabanas <acabanas@newrelic.com>
2022-11-18 09:11:32 +0100

    Modify entrypoint.sh

--- exporters/redis/entrypoint.sh
+++ exporters/redis/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-count= $(/usr/local/prometheus-exporters/bin/nri-redis -short_running -config_path=/usr/local/prometheus-exporters/bin/redis-config.yml)
-echo $count
+
+$(/usr/local/prometheus-exporters/bin/nri-redis -short_running -config_path=/usr/local/prometheus-exporters/bin/redis-config.yml)
