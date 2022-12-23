0c5e1fa98c29720793150f36cc9c7b4641a8addf
Juan Manuel Perez <jperezflorez@newrelic.com>
2022-12-23 11:39:22 +0100

    fix package naming

--- scripts/package.sh
+++ scripts/package.sh
@@ -8,7 +8,7 @@ integration=$2
 goos=$3
 
 integration_dir="${root_dir}/exporters/${integration}"
-exporters_doc_dir="${integration_dir}/target"
+integration_target="${integration_dir}/target"
 goreleaser_file="${root_dir}/scripts/pkg/.goreleaser.yml"
 goreleaser_file_template="${goreleaser_file}.template"
 
@@ -18,7 +18,7 @@ loadVariables "${integration_dir}"/exporter.yml
 
 echo "Package configurator"
 
-echo "Downloading the license and placing it under ${exporters_doc_dir}/${integration}-LICENSE"
+echo "Downloading the license and placing it under ${integration_target}/${integration}-LICENSE"
 tmp_dir=$(mktemp -d)
 filename="${EXPORTER_LICENSE_PATH}"
 git clone "${EXPORTER_REPO_URL}" "${tmp_dir}"
@@ -27,12 +27,19 @@ if [ ! -f "${tmp_license_file}" ]; then
   echo "Cannot find a LICENSE file called ${filename} in the exporter repo";
   exit 1;
 fi
-cp "${tmp_license_file}" "${exporters_doc_dir}/${integration}-LICENSE"
+cp "${tmp_license_file}" "${integration_target}/${integration}-LICENSE"
 rm -rf "${tmp_dir}"
 
 echo "Packaging"
+mkdir -pv "${integration_target}/packages"
 if [ "$goos" == "windows" ]; then
     powershell.exe -file "${root_dir}/scripts/win_msi_build.ps1" -arch amd64 -exporterName ${NAME} -version ${VERSION} -exporterGUID ${EXPORTER_GUID} -upgradeGUID ${UPGRADE_GUID} -licenseGUID ${LICENSE_GUID}
+
+    # Need to know where the packages are going to be
+    echo finding MSIs
+    find . -name \*.msi
+    echo finding everything
+    find .
 else
     cp "${goreleaser_file_template}" "${goreleaser_file}"
     IFS=',' read -r -a goarchs <<< "$PACKAGE_LINUX_GOARCHS"
@@ -45,4 +52,6 @@ else
     GORELEASER_CURRENT_TAG=${VERSION} ${goreleaser_bin} release --config "${goreleaser_file}" --rm-dist --snapshot
     echo "Signing the packages"
     #bash ${root_dir}/scripts/sign.sh "${root_dir}" "${integration}"
+
+    cp "${root_dir}/dist/*.{rpm,deb,tar.gz}" ${integration_target}/packages
 fi
