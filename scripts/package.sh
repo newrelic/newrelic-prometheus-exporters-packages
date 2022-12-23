6acfc39300126c8efcb09d0ccd0be00f6c8b7d54
Juan Manuel Perez <jperezflorez@newrelic.com>
2022-12-23 11:54:48 +0100

    Fix naming and path expansion

--- scripts/package.sh
+++ scripts/package.sh
@@ -53,5 +53,5 @@ else
     echo "Signing the packages"
     #bash ${root_dir}/scripts/sign.sh "${root_dir}" "${integration}"
 
-    cp "${root_dir}/dist/*.{rpm,deb,tar.gz}" ${integration_target}/packages
+    cp "${root_dir}/dist/"*.{rpm,deb,tar.gz} ${integration_target}/packages
 fi
